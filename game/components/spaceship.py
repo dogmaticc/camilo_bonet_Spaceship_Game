60e106fb7d1613a1971e7f4a1d391bacff6e2527
CamiloBonetV <camilobonetv@gmail.com>
2023-07-14 23:15:50 -0500

    Second-Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -3,32 +3,52 @@ from pygame.sprite import Sprite
 
 from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 class Spaceship(Sprite):
+    SHIP_WIDTH = 40
+    SHIP_HEIGHT = 60
+    X_POS = (SCREEN_WIDTH // 2) - SHIP_WIDTH
+    Y_POS = 500
+    SHIP_SPEED = 10
+
     def __init__(self):
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image, (40, 60))
+        self.image = pygame.transform.scale(self.image, (self.SHIP_WIDTH, self.SHIP_HEIGHT))
         self.rect = self.image.get_rect()
-        self.rect.x = (SCREEN_WIDTH // 2) - 40
-        self.rect.y = 500
+        self.rect.x = self.Y_POS
+        self.rect.y = self.Y_POS
 
     def update(self, user_input):
+
         if user_input[pygame.K_LEFT]:
-            if self.rect.left > -40:
-                self.rect.x -= 10
-        elif user_input[pygame.K_RIGHT]:
-            if self.rect.right < SCREEN_WIDTH + 40:
-                self.rect.x += 10
-        elif user_input[pygame.K_UP]:
-            if self.rect.y > SCREEN_HEIGHT // 2:
-                self.rect.y -= 10
-        elif user_input[pygame.K_DOWN]:
-            if self.rect.y < SCREEN_HEIGHT -70:
-                self.rect.y += 10
+            self.move_left()
 
-        if self.rect.right > SCREEN_WIDTH + 39:
-            self.rect.left = 0
-        elif self.rect.left < -39:
-            self.rect.right = SCREEN_WIDTH 
+        if user_input[pygame.K_RIGHT]:
+            self.move_right()
 
+        if user_input[pygame.K_UP]:
+            self.move_up()
+
+        if user_input[pygame.K_DOWN]:
+            self.move_down()
+
+
+    def move_left(self):
+        self.rect.x -= self.SHIP_SPEED
+        if self.rect.x < 0:
+            self.rect.x = SCREEN_WIDTH - self.SHIP_WIDTH
+
+    def move_right(self):
+        self.rect.x += self.SHIP_SPEED
+        if self.rect.x > SCREEN_WIDTH:
+            self.rect.x = 0
+            
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT // 2:
+            self.rect.y -= self.SHIP_SPEED
+    
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT -70:
+            self.rect.y += self.SHIP_SPEED
+   
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
--- game/utils/constants.py
+++ game/utils/constants.py
@@ -27,4 +27,7 @@ BULLET = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_1.png"))
 BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_2.png"))
 ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
 
+SHIP_WIDTH = 40
+SHIP_HEIGHT = 60
+
 FONT_STYLE = 'freesansbold.ttf'
